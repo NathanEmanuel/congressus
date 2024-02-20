ecf0a798adf791892866ecdde05be695b22e2eb7
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-02-20 15:41:39 +0100

    split ClientTest into ClientTest and RawClientTest

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,41 +1,18 @@
 <?php
 
 use Compucie\Congressus\Client;
-use Compucie\Congressus\Request\Parameters;
-use Compucie\Congressus\Request\ParameterType\Path;
-use Compucie\Congressus\Request\ParameterType\Query;
 
 require_once '../vendor/autoload.php';
 
 $client = new Client(getenv("congressus"));
 
-$parameters = new Parameters();
-$parameters->add(Query::term, "Nathan Djojomoenawie");
-$data = $client->search_members($parameters);
+// Members
 
-$parameters = new Parameters();
-$parameters->add(Query::period_filter, [null, (time() + 2629800)]);
-$data = $client->list_events($parameters);
+// $member = $client->retrieve_member_by_id(69);
+// print_r($member);
 
-$parameters = new Parameters();
-$parameters->add(Path::obj_id, 282676);
-$data = $client->retrieve_member($parameters);
 
-$parameters = new Parameters();
-$parameters->add(Path::obj_id, "282676");
-$data = $client->retrieve_member($parameters);
+// Products
 
-$data = $client->list_members();
-
-$data = $client->list_groups();
-
-$data = $client->list_group_memberships();
-
-$data = $client->list_events();
-
-$data = $client->list_products();
-
-$data = $client->list_products_folders_recursive();
-print_r($data);
-
-$data = $client->list_products_folders();
+$products = $client->list_products_in_folder(5427);
+print_r($products);
---
+++ tests/RawClientTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Compucie\Congressus\RawClient;
+use Compucie\Congressus\Request\Parameters;
+use Compucie\Congressus\Request\ParameterType\Path;
+use Compucie\Congressus\Request\ParameterType\Query;
+
+require_once '../vendor/autoload.php';
+
+$client = new RawClient(getenv("congressus"));
+
+$parameters = new Parameters();
+$parameters->add(Query::term, "Nathan Djojomoenawie");
+$data = $client->search_members($parameters);
+
+$parameters = new Parameters();
+$parameters->add(Query::period_filter, [null, (time() + 2629800)]);
+$data = $client->list_events($parameters);
+
+$parameters = new Parameters();
+$parameters->add(Path::obj_id, 282676);
+$data = $client->retrieve_member($parameters);
+
+$parameters = new Parameters();
+$parameters->add(Path::obj_id, "282676");
+$data = $client->retrieve_member($parameters);
+
+$data = $client->list_members();
+
+$data = $client->list_groups();
+
+$data = $client->list_group_memberships();
+
+$data = $client->list_events();
+
+$data = $client->list_products();
+
+$data = $client->list_products_folders_recursive();
+print_r($data);
+
+$data = $client->list_products_folders();
