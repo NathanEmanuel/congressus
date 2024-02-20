504f6e5a0dd6c1dfc0b3bdbcbcd6919fc890f6e8
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-02-20 13:41:48 +0100

    add all path parameter types

--- src/request/ParameterType/Path.php
+++ src/request/ParameterType/Path.php
@@ -6,6 +6,10 @@ use Compucie\Congressus\Request\ParameterType\ParameterTypeInterface;
 
 enum Path: string implements ParameterTypeInterface
 {
+    case event_id = "event_id";
+    case log_entry_id = "log_entry_id";
+    case member_id = "member_id";
+    case membership_status_id = "membership_status_id";
     case obj_id = "obj_id";
 
     public function get_value(): string
