3e1bb389e7994518c709a6a53d8778c5ed5e9299
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-07-16 23:33:29 +0200

    remove Page related code

--- src/RawClient.php
+++ src/RawClient.php
@@ -39,13 +39,6 @@ class RawClient extends GuzzleHttpClient
         }
 
         // deserialize response
-        $isPaginated = str_contains(get_class($responseType), "Pagination");
-        if ($isPaginated) {
-            $get_calling_method = function () {
-                return debug_backtrace()[2]["function"];
-            };
-            return new Page(get_class($responseType), $responseBody, $get_calling_method(), $request->getArgs());
-        }
         return ObjectSerializer::deserialize($responseBody, get_class($responseType));
     }
 
@@ -59,57 +52,6 @@ class RawClient extends GuzzleHttpClient
         $resource = \GuzzleHttp\Psr7\Utils::tryFopen($filePath, 'w');
         $stream = \GuzzleHttp\Psr7\Utils::streamFor($resource);
         $this->request($request->getMethod(), $request->getPath(), ["sink" => $stream]); // send() does not seem to work
-    }
-
-    /**
-     * Request next page.
-     * @param   Page                $page   The current page
-     * @return  Page                        The next page
-     * @throws  NoNextPageException
-     */
-    public function nextPage(Page $page): Page
-    {
-        if ($page->hasNext()) {
-            $parameters = $page->getParameters();
-            $parameters["page"] = $page->getNextPageNumber();
-            return call_user_func(array($this, $page->getCaller()), ...$parameters);
-        } else {
-            throw new NoNextPageException();
-        }
-    }
-
-    /**
-     * Request subsequent pages.
-     * @param   Page    $page   The current page
-     * @param   int     $max    The maximum amount of pages to request
-     * @return  Page[]          Array of the subsequent pages
-     */
-    public function nextPages(Page $page, int $max): array
-    {
-        $pages = array();
-        for ($i = 0; $i < $max; $i++) {
-            try {
-                $page = $this->nextPage($page);
-                array_push($pages, $page);
-            } catch (NoNextPageException) {
-                break;
-            }
-        }
-        return $pages;
-    }
-
-    /**
-     * Request subsequent pages and add their data to the data array.
-     * @param   array   $data   Data array.
-     * @param   Page    $page   The current page.
-     * @param   int     $max    The maximum amount of pages to request.
-     */
-    public function addDataFromNextPages(array &$data, Page $page, int $max): void
-    {
-        $pages = $this->nextPages($page, $max);
-        foreach ($pages as $page) {
-            $data = array_merge($data, $page->getData());
-        }
     }
 
     private function isRequestingAllowed($page, int $limit): bool
